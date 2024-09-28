68b45ec02f2811c1a3297daefc5e1ae3e2f5f802
balumahendra-21 <balumahendrasakile@gmail.com>
2024-09-28 11:58:37 +0530

    deployed

--- components/All.jsx
+++ components/All.jsx
@@ -4,23 +4,93 @@ export const All = () => {
   return (
     <div className='all-tab'>
       <div className="muscles">
-        <div className="m1"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497902/chest-1_ifcuvs-removebg-preview_gvnhb2.png" alt="" /><span> chest</span></div>
-        <div className="m2"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497907/lats_lrkcif-removebg-preview_wvrdmf.png" alt="" />lats</div>
-        <div className="m3"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497909/triceps_gjkht0-removebg-preview_qaly0w.png" alt="" />tricep</div>
-        <div className="m4"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497903/bicep_difpw2-removebg-preview_afy3r9.png" alt="" />bicep</div>
-        <div className="m5"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497903/anterior_delts_plsncu-removebg-preview_onvaym.png" alt="" />anterior delts</div>
-        <div className="m6"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497908/quads_cszu6q-removebg-preview_hkgoem.png" alt="" />quads</div>
-        <div className="m7"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497907/traps_psil80-removebg-preview_hsqrpl.png" alt="" />traps</div>
-        <div className="m8"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497904/gluteus_gn6mn8-removebg-preview_pffrsi.png" alt="" />gluteus</div>
-        <div className="m9"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497902/gastrocnemius_hudn3u-removebg-preview_n6eoca.png" alt="" />gastrocnemius</div>
-        <div className="m10"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497907/posterior_delt_o0bdfn-removebg-preview_uearv7.png" alt="" />Posterior delt</div>
-        <div className="m11"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497902/adductors_xlozk2-removebg-preview_pykaip.png" alt="" />adductors</div>
-        <div className="m12"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497902/abs_eoeeem-removebg-preview_hckomn.png" alt="" />abs</div>
-        <div className="m13"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497905/hamstrings_nwpqod-removebg-preview_brum3o.png" alt="" />hamstrings</div>
-        <div className="forearms"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497902/forearms_utxezj-removebg-preview_n6kvug.png" alt="" />forearms</div>
-        <p className="m14"></p>
-        <p className="m15"></p>
-        <p className="m16"></p>
+            <div class="notification">
+                <div class="notiglow"></div>
+                <div class="notiborderglow"></div>
+                <div class="notititle"><h3>Chest</h3></div>
+                <img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497902/chest-1_ifcuvs-removebg-preview_gvnhb2.png" alt="" />
+                <ion-icon name="chevron-forward-outline"></ion-icon>
+            </div>
+            <div class="notification">
+                <div class="notiglow"></div>
+                <div class="notiborderglow"></div>
+                <div class="notititle"><h3>Lats</h3></div>
+                <img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497907/lats_lrkcif-removebg-preview_wvrdmf.png" alt="" />
+                <ion-icon name="chevron-forward-outline"></ion-icon>
+            </div>
+            <div class="notification">
+                <div class="notiglow"></div>
+                <div class="notiborderglow"></div>
+                <div class="notititle"><h3>Triceps</h3></div>
+                <img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497909/triceps_gjkht0-removebg-preview_qaly0w.png" alt="" />                <ion-icon name="chevron-forward-outline"></ion-icon>
+            </div>
+            <div class="notification">
+                <div class="notiglow"></div>
+                <div class="notiborderglow"></div>
+                <div class="notititle"><h3>Biceps</h3></div>
+                <img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497903/bicep_difpw2-removebg-preview_afy3r9.png" alt="" />                <ion-icon name="chevron-forward-outline"></ion-icon>
+            </div>
+            <div class="notification">
+                <div class="notiglow"></div>
+                <div class="notiborderglow"></div>
+                <div class="notititle"><h3>Anterior Delts</h3></div>
+                <img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497903/anterior_delts_plsncu-removebg-preview_onvaym.png" alt="" />                <ion-icon name="chevron-forward-outline"></ion-icon>
+            </div>
+            <div class="notification">
+                <div class="notiglow"></div>
+                <div class="notiborderglow"></div>
+                <div class="notititle"><h3>Posterior delts</h3></div>
+                <img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497907/posterior_delt_o0bdfn-removebg-preview_uearv7.png" alt="" />                <ion-icon name="chevron-forward-outline"></ion-icon>
+            </div>
+            <div class="notification">
+                <div class="notiglow"></div>
+                <div class="notiborderglow"></div>
+                <div class="notititle"><h3>Traps</h3></div>
+                <img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497907/traps_psil80-removebg-preview_hsqrpl.png" alt="" />                <ion-icon name="chevron-forward-outline"></ion-icon>
+            </div>
+            <div class="notification">
+                <div class="notiglow"></div>
+                <div class="notiborderglow"></div>
+                <div class="notititle"><h3>Quads</h3></div>
+                <img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497908/quads_cszu6q-removebg-preview_hkgoem.png" alt="" />                <ion-icon name="chevron-forward-outline"></ion-icon>
+            </div>
+            <div class="notification">
+                <div class="notiglow"></div>
+                <div class="notiborderglow"></div>
+                <div class="notititle"><h3>Hamstrings</h3></div>
+                <img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497905/hamstrings_nwpqod-removebg-preview_brum3o.png" alt="" />                <ion-icon name="chevron-forward-outline"></ion-icon>
+            </div>
+            <div class="notification">
+                <div class="notiglow"></div>
+                <div class="notiborderglow"></div>
+                <div class="notititle"><h3>Glutues</h3></div>
+                <img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497904/gluteus_gn6mn8-removebg-preview_pffrsi.png" alt="" />                <ion-icon name="chevron-forward-outline"></ion-icon>
+            </div>
+            <div class="notification">
+                <div class="notiglow"></div>
+                <div class="notiborderglow"></div>
+                <div class="notititle"><h3>Adductors</h3></div>
+                <img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497902/adductors_xlozk2-removebg-preview_pykaip.png" alt="" />                <ion-icon name="chevron-forward-outline"></ion-icon>
+            </div>
+            <div class="notification">
+                <div class="notiglow"></div>
+                <div class="notiborderglow"></div>
+                <div class="notititle"><h3>Gastrocnemius</h3></div>
+                <img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497902/gastrocnemius_hudn3u-removebg-preview_n6eoca.png" alt="" />                <ion-icon name="chevron-forward-outline"></ion-icon>
+            </div>
+            <div class="notification">
+                <div class="notiglow"></div>
+                <div class="notiborderglow"></div>
+                <div class="notititle"><h3>Abs</h3></div>
+                <img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497902/abs_eoeeem-removebg-preview_hckomn.png" alt="" />                <ion-icon name="chevron-forward-outline"></ion-icon>
+            </div>
+         
+            <div class="notification">
+                <div class="notiglow"></div>
+                <div class="notiborderglow"></div>
+                <div class="notititle"><h3>Forearms</h3></div>
+                <img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497902/forearms_utxezj-removebg-preview_n6kvug.png" alt="" />                <ion-icon name="chevron-forward-outline"></ion-icon>
+            </div>
       </div>
     </div>
   )
