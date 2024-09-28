618a836dcb8dc1c12148501780597ccbd23fe057
balumahendra-21 <balumahendrasakile@gmail.com>
2024-09-28 10:07:48 +0530

    logos updated

--- components/All.jsx
+++ components/All.jsx
@@ -4,19 +4,20 @@ export const All = () => {
   return (
     <div className='all-tab'>
       <div className="muscles">
-        <div className="m1"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727428328/chest_g68ugc.png" alt="" /><span> chest</span></div>
-        <div className="m2"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727444692/lats_lrkcif.png" alt="" />lats</div>
-        <div className="m3"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727444692/triceps_gjkht0.png" alt="" />tricep</div>
-        <div className="m4"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727444691/bicep_difpw2.png" alt="" />bicep</div>
-        <div className="m5"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727444691/anterior_delts_plsncu.png" alt="" />anterior delts</div>
-        <div className="m6"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727444692/quads_cszu6q.png" alt="" />quads</div>
-        <div className="m7"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727444692/traps_psil80.png" alt="" />traps</div>
-        <div className="m8"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727444692/gluteus_gn6mn8.png" alt="" />gluteus</div>
-        <div className="m9"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727444692/gastrocnemius_hudn3u.png" alt="" />gastrocnemius</div>
-        <div className="m10"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727444692/posterior_delt_o0bdfn.png" alt="" />Posterior delt</div>
-        <div className="m11"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727444691/adductors_xlozk2.png" alt="" />adductors</div>
-        <div className="m12"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727444691/abs_eoeeem.png" alt="" />abs</div>
-        <div className="m13"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727445256/hamstrings_nwpqod.png" alt="" />hamstrings</div>
+        <div className="m1"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497902/chest-1_ifcuvs-removebg-preview_gvnhb2.png" alt="" /><span> chest</span></div>
+        <div className="m2"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497907/lats_lrkcif-removebg-preview_wvrdmf.png" alt="" />lats</div>
+        <div className="m3"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497909/triceps_gjkht0-removebg-preview_qaly0w.png" alt="" />tricep</div>
+        <div className="m4"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497903/bicep_difpw2-removebg-preview_afy3r9.png" alt="" />bicep</div>
+        <div className="m5"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497903/anterior_delts_plsncu-removebg-preview_onvaym.png" alt="" />anterior delts</div>
+        <div className="m6"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497908/quads_cszu6q-removebg-preview_hkgoem.png" alt="" />quads</div>
+        <div className="m7"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497907/traps_psil80-removebg-preview_hsqrpl.png" alt="" />traps</div>
+        <div className="m8"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497904/gluteus_gn6mn8-removebg-preview_pffrsi.png" alt="" />gluteus</div>
+        <div className="m9"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497902/gastrocnemius_hudn3u-removebg-preview_n6eoca.png" alt="" />gastrocnemius</div>
+        <div className="m10"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497907/posterior_delt_o0bdfn-removebg-preview_uearv7.png" alt="" />Posterior delt</div>
+        <div className="m11"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497902/adductors_xlozk2-removebg-preview_pykaip.png" alt="" />adductors</div>
+        <div className="m12"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497902/abs_eoeeem-removebg-preview_hckomn.png" alt="" />abs</div>
+        <div className="m13"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497905/hamstrings_nwpqod-removebg-preview_brum3o.png" alt="" />hamstrings</div>
+        <div className="forearms"><img src="https://res.cloudinary.com/dutz70yxy/image/upload/v1727497902/forearms_utxezj-removebg-preview_n6kvug.png" alt="" />forearms</div>
         <p className="m14"></p>
         <p className="m15"></p>
         <p className="m16"></p>
