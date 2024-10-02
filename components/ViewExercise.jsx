c5c4580ceb769822d2d98a528a2685096d5385fc
pradeep <bolla.h678@gmail.com>
2024-10-02 11:41:27 +0530

    'something'

--- components/ViewExercise.jsx
+++ components/ViewExercise.jsx
@@ -7,15 +7,14 @@ export const ViewExercise = (props) => {
     const [field,setfield]=useState(false)
     const [name,setName]=useState('')
     const [index,setIndex]=useState(0)
+    const [index2,setIndex2]=useState(0)
     const [warning,setWarning]=useState('')
-    const [edit,setEdit]=useState(false)
+    const [coordinates,setCoordinates]=useState(0)
     const navigate=useNavigate()
+    const [enable,setEnable] = useState(false)
     const [refresh,setRefresh]=useState(false)
+    const [set,setSet]=useState(false);
     useEffect(()=>{
-        const editor=document.getElementsByClassName('editor')[index]
-        if(editor){
-            editor.style.display='none'
-        }
         const getdata=async()=>{
             const result=await getData(record.id)
             setRecord(result)
@@ -25,66 +24,95 @@ export const ViewExercise = (props) => {
     },[field,refresh])
     return (
     <>
+    {enable && <div className="editor" style={{top:coordinates+5}} tabIndex={0} onBlur={()=>{
+        const editor=document.getElementsByClassName('editor')[index]
+        if (editor) {
+                editor.style.display = 'none';
+            } 
+            setEnable(false)
+        }}>
+        {!set &&<><div className="editbtn">
+            <input type="text"  placeholder='Rename..' onBlur={(e)=>{
+            const rename=e.target.value
+            if(rename!==''){
+                const updatedata=record
+                updatedata.data.exercises[index].name=rename
+                updateData(updatedata)
+                .then(() => console.log("Record updated successfully"))
+                .catch(error => console.error("Failed to update record: ", error));
+                setEnable(false)
+                setRefresh(refresh?false:true)
+            }}}/><ion-icon name="create"></ion-icon></div><hr /></>} 
+        <div className="delete" onClick={()=>{
+            const updatedata=record
+            if(set){
+                updatedata.data.exercises[index].sets.splice(index2,1)
+            }else{
+                updatedata.data.exercises.splice(index2,1)
+            }
+            updateData(updatedata)
+            .then(() => console.log("Record updated successfully"))
+            .catch(error => console.error("Failed to update record: ", error));
+            setEnable(false)
+            setRefresh(refresh?false:true)
+        }}><span>Delete</span><ion-icon name="trash"></ion-icon></div>
+    </div>}
     <div className='view'>
         <div className="heading">
-            <ion-icon name="chevron-back-outline" onClick={()=>{navigate('/all')}}></ion-icon>
+            <ion-icon name="chevron-back-outline" onClick={()=>{(set)?setSet(false):navigate('/all')}}></ion-icon>
             <img src={props.data.data.imgurl} alt="" />
             <h3>{props.data.id}</h3>
         </div>
-        <h3>Exercises</h3>
-        {record &&<div className="exercises">
+        <h3>{set?<>{record.data.exercises[index].name}</>:<>Exercises</>}</h3>
+        {set && <div className='showset'>
+            {record.data.exercises[index] &&<>
+            <div className="set-container">
+                {record.data.exercises[index].sets.map((set,index)=>{
+                    return <>
+                    <div className="e" key={index}>
+                    <ion-icon name="stopwatch"></ion-icon><pre> </pre>
+                        set-{index+1}
+                        <div onClick={(e)=>{
+                                    setIndex2(index)
+                                    setEnable(enable?false:true)
+                                    setCoordinates(e.clientY)
+                        }} className="edit"><ion-icon name="ellipsis-vertical"></ion-icon></div>
+                    </div>
+                    </>
+                })}
+                <div className="e" onClick={()=>{
+                record.data.exercises[index].sets.push({working:{weight:0,reps:0},dropset:[{weight:0,reps:0}]})
+                updateData(record)
+                .then(() => console.log("Record updated successfully"))
+                .catch(error => console.error("Failed to update record: ", error));
+                setRefresh(refresh?false:true)
+                }}>
+                <ion-icon name="add-outline"></ion-icon>
+                <p>add set</p>
+            </div>
+            </div></>}
+        </div>}
+        {record && !set && <div className="exercises">
             {
                 record.data.exercises.length && <>{
                 record.data.exercises.map((exercise,index)=>{
                         return  <>
-                            <div className="e" key={index}>
-                                <div className="editor" tabIndex={0} autoFocus onBlur={()=>{
-                                    const editor=document.getElementsByClassName('editor')[index]
-                                    if (editor) {
-                                            editor.style.display = 'none';
-                                        } 
-                                }} key={index} id={'l'+index}>
-                                    <div className="editbtn">
-                                        <input type="text" autoFocus placeholder='Rename..' onBlur={(e)=>{
-                                        const rename=e.target.value
-                                        if(rename!==''){
-                                            const updatedata=record
-                                            updatedata.data.exercises[index].name=rename
-                                            updateData(updatedata)
-                                            .then(() => console.log("Record updated successfully"))
-                                            .catch(error => console.error("Failed to update record: ", error));
-                                            setRefresh(refresh?false:true)
-                                        }}}/><ion-icon name="create"></ion-icon></div> <hr />
-                                    <div className="delete" onClick={()=>{
-                                        const updatedata=record
-                                        updatedata.data.exercises.splice(index,1)
-                                        updateData(updatedata)
-                                        .then(() => console.log("Record updated successfully"))
-                                        .catch(error => console.error("Failed to update record: ", error));
-                                        setRefresh(refresh?false:true)
-                                    }}><span>Delete</span><ion-icon name="trash"></ion-icon></div>
-                                </div>
+                            <div className="e" key={index} >
                                 <ion-icon name="barbell-sharp"></ion-icon><pre> </pre>
-                                <p>{exercise.name}</p> 
-                                <div onClick={()=>{
+                                <p onClick={()=>{
+                                setSet(set?false:true)
+                                setIndex(index)}}>{exercise.name}</p> 
+                                <div onClick={(e)=>{
+                                    setEnable(enable?false:true)
                                     setIndex(index)
-                                    const editor=document.getElementsByClassName('editor')[index]
-                                    if (editor) {
-                                        if (editor.style.display === 'none' || editor.style.display === '') {
-                                          editor.style.display = 'block'; // Show editor
-                                        } else {
-                                          editor.style.display = 'none'; // Hide editor
-                                        }
-                                    } else {
-                                        console.error(`Element with id 'l${index}' not found`);
-                                    }
+                                    setCoordinates(e.clientY)
                                 }} className="edit"><ion-icon name="ellipsis-vertical"></ion-icon></div>
                             </div>
                                 </>})}</>
             }
             {field&& <div className={'e '+warning} >
                 <ion-icon name="barbell-sharp"></ion-icon>
-                <input type="text" name="" id="" placeholder='name...' autoFocus onChange={(e)=>{setName(e.target.value)}} onBlur={()=>{
+                <input type="text" name="" id="" placeholder='name...' autoFocus onFocus={()=>{setEnable(false)}} onChange={(e)=>{setName(e.target.value)}} onBlur={()=>{
             if(field){
                 if(name===''){  
                     setWarning('t')
